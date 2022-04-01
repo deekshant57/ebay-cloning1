c3321ec6301f99fcf6f3eeb2b9c352e84ed1a878
deekshant57 <deekshant57@gmail.com>
2022-04-01 23:23:28 +0530

    Updated

--- scripts/password.js
+++ scripts/password.js
@@ -1,22 +1,44 @@
-document.querySelector("form").addEventListener("submit",signin);
-var email_data=JSON.parse(localStorage.getItem("login_Datas"));
+// var email_data = JSON.parse(localStorage.getItem("login_Datas"));
+document.querySelector("form").addEventListener("submit", signin);
+// var email_data=JSON.parse(localStorage.getItem("login_Datas"));
 
-var count=0;
-function signin(event){
-    event.preventDefault();
-    var pass=document.getElementById("password").value;
-    
-  
-    for(var i=0;i<email_data.length;i++){
-        if(email_data[i].pass==pass){
-            
-            window.location.href="home.html";
-        }
-        else{
-            count++;
-        }
-    }
-    if(count==email_data.length){
-        alert("invalid Credentials")
-    }
+var count = 0;
+async function signin(event) {
+  event.preventDefault();
+
+  let data = {
+    email: localStorage.getItem("email"),
+    password: document.getElementById("password").value,
+  };
+  console.log(data);
+  data = JSON.stringify(data);
+
+  let result = await fetch("http://localhost:4000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+
+  let response = await result.json();
+
+  console.log(response);
+
+  if (response.token) {
+    // console.log("loggedin");
+    window.location.href = "home.html";
+  } else {
+    alert(response.message);
+  }
+
+  //   for (var i = 0; i < email_data.length; i++) {
+  //     if (email_data[i].pass == pass) {
+  //       window.location.href = "home.html";
+  //     } else {
+  //       count++;
+  //     }
+  //   }
+  //   if (count == email_data.length) {
+  //     alert("invalid Credentials");
 }
--- scripts/signin.js
+++ scripts/signin.js
@@ -4,10 +4,10 @@
 // function signin(event){
 //     event.preventDefault();
 //     var email=document.getElementById("email").value;
-  
+
 //     for(var i=0;i<email_data.length;i++){
 //         if(email_data[i].mail==email){
-            
+
 //             window.location.href="password.html";
 //         }
 //         else{
@@ -19,43 +19,36 @@
 //     }
 // }
 
+let sumbit = document.getElementById("submit");
+submit.addEventListener("click", signin);
 
-  let sumbit = document.getElementById("submit");
-  submit.addEventListener("click", signin);
+async function signin() {
+  event.preventDefault();
+  console.log("hello!!!");
+  let email = document.getElementById("email").value;
+  localStorage.setItem("email", email);
+  window.location.href = "password.html";
 
-  async function signin() {
-    console.log("hello!!!");
+  // let result = await fetch("http://localhost:4000/login", {
+  //   method: "POST",
+  //   body: data,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 
-    let data = {
-        email : document.getElementById('email').value,
-        pass : document.getElementById('password').value,
-    };
+  // let response = await result.json();
 
-    data = JSON.stringify(data);
-    console.log(data);
-
-    let result = await fetch("http://localhost:4000/login", {
-      method: "POST",
-      body: data,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    let response = await result.json();
-
-    console.log(response);
-     if(response.status==0){
-     alert("Please Registered First");
-     window.location.href="personalAc.html";
-     }
-    else if(response.status==1){
-      alert("Please enter valid email or password");
-     }
-     else if(response.status==2){
-      alert("You are sucessfulley log in");
-      window.location.href="home.html";
-    localStorage.setItem("user",JSON.stringify(response.user))
-      console.log(response.user);
-     }
-  };  
+  // console.log(response);
+  // if (response.status == 0) {
+  //   alert("Please Registered First");
+  //   window.location.href = "personalAc.html";
+  // } else if (response.status == 1) {
+  //   alert("Please enter valid email or password");
+  // } else if (response.status == 2) {
+  //   alert("You are sucessfulley log in");
+  //   window.location.href = "home.html";
+  //   localStorage.setItem("user", JSON.stringify(response.user));
+  //   console.log(response.user);
+  // }
+}
